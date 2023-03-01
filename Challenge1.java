2c31bcb9d60e97dffc21febfab518c10d4053cf7
unknown <caturanggraena@gmail.com>
2023-03-01 20:48:46 +0700

    tugas catur

---
+++ Challenge1.java
@@ -0,0 +1,196 @@
+import java.util.Scanner;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        MainMenu();
+    }
+
+    private static void MainMenu() {
+        Scanner input = new Scanner(System.in);
+        int pilihan = -1; // value defaultnya -1
+
+        // main menu
+        while (pilihan != 0) {
+            System.out.println("----------------------------------------");
+            System.out.println("  Kalkulator Penghitung Luas dan Volum  ");
+            System.out.println("----------------------------------------");
+            System.out.println("                  Menu                  ");
+            System.out.println("1. Hitung luas bidang");
+            System.out.println("2. Hitung Volum");
+            System.out.println("0. Tutup Aplikasi");
+            System.out.print("Masukkan pilihan anda : ");
+            pilihan = input.nextInt();
+
+            // case buat pilihan input main menu
+            switch (pilihan) {
+                case 1:
+                    LuasBidang(); // forward ke void LuasBidang()
+                    break;
+                case 2:
+                    VolumBidang(); // forward ke void VolumBidang()
+                    break;
+                case 0:
+                    System.out.println("Terimakasih telah menggunakan kalkulator!!");
+                    System.exit(0); // close program
+
+                default:
+                    System.out.println("Inputan tidak valid!"); // output kalo input ga sesuai dari case yang tersedia
+                    break;
+            }
+        }
+    }
+
+    private static void LuasBidang() {
+        Scanner input = new Scanner(System.in);
+        int pilihan = -1;
+        double sisiP, panjangPP, lebarPP, alas, tinggi, jariJ, hasil; // pake double biar nilainya bisa desimal
+        String home;
+
+        // menu luas
+        while (pilihan != 0) {
+            System.out.println("----------------------------------------");
+            System.out.println("            Hitung luas Bidang          ");
+            System.out.println("----------------------------------------");
+            System.out.println("1. Persegi");
+            System.out.println("2. Lingkaran");
+            System.out.println("3. Segitiga");
+            System.out.println("4. Persegi Panjang");
+            System.out.println("0. Kembali ke menu sebelumnya");
+            System.out.print("Masukkan pilihan anda : ");
+            pilihan = input.nextInt();
+
+            // case buat pilihan input luas bidang
+            switch (pilihan) {
+                case 1:
+                    System.out.println("----------------------------------------");
+                    System.out.println("          Anda Memilih Persegi          ");
+                    System.out.println("----------------------------------------");
+                    System.out.print("Masukkan sisi : ");
+                    sisiP = input.nextInt();
+                    hasil = sisiP + sisiP;
+                    System.out.println("Processing...");
+                    System.out.println("Luas persegi adalah " + hasil + " cm^2");
+
+                    // Input random bakal balik ke void LuasBidang()
+                    System.out.println("Tekan apa saja untuk kembali ke menu");
+                    home = input.next();
+                    LuasBidang();
+                case 2:
+                case 3:
+                case 4:
+                    System.out.println("----------------------------------------");
+                    System.out.println("     Anda Memilih Persegi panjang       ");
+                    System.out.println("----------------------------------------");
+                    double panjang, lebar, luas;
+
+                    System.out.print("Input panjang persegi panjang: ");
+                    panjang = input.nextDouble();
+
+                    System.out.print("Input lebar persegi panjang: ");
+                    lebar = input.nextDouble();
+
+                    luas = panjang * lebar;
+                    luas = Math.round(luas * 100) / 100.0;
+
+                    System.out.println("Luas persegi panjang: " + luas);
+
+                    // Input random bakal balik ke void LuasBidang()
+                    System.out.println("Tekan apa saja untuk kembali ke menu");
+                    home = input.next();
+                    LuasBidang();
+                case 0:
+                    System.out.println("Kembali ke menu utama...");
+                    MainMenu();
+                default:
+                    System.out.println("Inputan tidak valid!"); // output kalo input ga sesuai dari case yang tersedia
+                    break;
+            }
+        }
+    }
+
+    private static void VolumBidang() {
+        Scanner input = new Scanner(System.in);
+        double sisiK, panjangB, lebarB, tinggiB, tinggiT, jariT, hasil;
+        int pilihan = -1;
+        String home;
+
+        while (pilihan != 0) {
+            System.out.println("----------------------------------------");
+            System.out.println("           Hitung volum Bidang          ");
+            System.out.println("----------------------------------------");
+            System.out.println("1. Kubus");
+            System.out.println("2. Balok");
+            System.out.println("3. Tabung");
+            System.out.println("0. Kembali ke menu sebelumnya");
+            System.out.print("Masukkan pilihan anda : ");
+            pilihan = input.nextInt();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.println("----------------------------------------");
+                    System.out.println("     Anda Memilih Kubus                 ");
+                    System.out.println("----------------------------------------");
+                    double sisi;
+
+                    System.out.print("Masukkan Sisi : ");
+                    sisi = input.nextDouble();
+
+                    hasil = sisi * sisi * sisi;
+
+                    System.out.println("Hasil Perhitungannya adalah : " + hasil);
+                    System.out.println("Tekan apa saja untuk kembali ke menu");
+                    home = input.next();
+                    VolumBidang();
+
+                case 2:
+                    System.out.println("----------------------------------------");
+                    System.out.println("             Anda Memilih Balok         ");
+                    System.out.println("----------------------------------------");
+                    double p, l, t, v;
+
+                    System.out.println("Masukan Panjang: ");
+                    p = input.nextDouble();
+                    System.out.println("Masukan Lebar: ");
+                    l = input.nextDouble();
+                    System.out.println("Masukan Tinggi: ");
+                    t = input.nextDouble();
+                    v = p * l * t;
+
+                    System.out.println("----------------------------------------");
+                    System.out.println("Hasilnya adalah " + v);
+                    System.out.println("Tekan apa saja untuk kembali ke menu");
+                    home = input.next();
+                    VolumBidang();
+
+                case 3:
+                    System.out.println("----------------------------------------");
+                    System.out.println("            Anda Memilih Tabung         ");
+                    System.out.println("----------------------------------------");
+
+                    double hasil1, hasil2, d, t1;
+                    double phi = 3.14;
+
+                    System.out.print("Masukkan Diameter Tabung: ");
+                    d = input.nextDouble();
+                    System.out.print("Masukkan Panjang Tabung: ");
+                    t1 = input.nextDouble();
+
+                    hasil2 = d / 2;
+                    hasil1 = (phi * hasil2 * hasil2 * t1);
+
+                    System.out.println("Hasil Perhitungannya adalah : " + hasil1);
+                    System.out.println("Tekan apa saja untuk kembali ke menu");
+                    home = input.next();
+                    VolumBidang();
+                case 0:
+                    System.out.println("Kembali ke menu utama...");
+                    MainMenu();
+                    break;
+                default:
+                    System.out.println("Inputan tidak valid!"); // output kalo input ga sesuai dari case yang tersedia
+                    break;
+            }
+        }
+
+    }
+}
